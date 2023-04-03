57aacbfa588c8847e8b50f1cafd28b0b6ff5339f
Julia Stamborovski <juliashizuko66@gmail.com>
2023-04-03 20:37:48 -0300

    Entidades JPA

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,6 +1,6 @@
 package med.voll.api.controller;
 
-import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.DadosCadastroMedico; //DTO
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
