04fe91eadb934ec62f047c572c8f570b0429fb48
Julia Stamborovski <juliashizuko66@gmail.com>
2023-04-05 23:15:58 -0300

    Validação com Bean Validation

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,5 +1,6 @@
 package med.voll.api.controller;
 
+import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedico; //DTO
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
@@ -21,7 +22,7 @@ public class MedicoController {
     @PostMapping  //verbo post
     @Transactional
     //requisição do tipo post para a URL /medicos, para que ative o método cadastrar da classe MedicoController
-    public void cadastrar(@RequestBody DadosCadastroMedico dados) {
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
         repository.save(new Medico(dados));
     }
 
--- src/main/java/med/voll/api/medico/DadosCadastroMedico.java
+++ src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -1,6 +1,26 @@
 package med.voll.api.medico;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import med.voll.api.endereco.DadosEndereco;
 
-public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade, DadosEndereco endereco) {
+public record DadosCadastroMedico(
+
+        @NotBlank
+        String nome,
+        @NotBlank
+        @Email
+        String email,
+        @NotBlank
+        @Pattern(regexp = "\\d{4,6}") //expressão regular
+        String crm,
+        @NotNull
+        Especialidade especialidade,
+        @NotNull
+        @Valid //validar o DTO
+        DadosEndereco endereco
+) {
 }
