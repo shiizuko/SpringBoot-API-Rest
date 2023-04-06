e2aa58c395fcf5758b4b3094854bd34bfab00ebd
Julia Stamborovski <juliashizuko66@gmail.com>
2023-04-05 22:53:26 -0300

    Migrations com Flyway

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -4,6 +4,7 @@ import med.voll.api.medico.DadosCadastroMedico; //DTO
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ public class MedicoController {
 
     //métodos para funcionalidade
     @PostMapping  //verbo post
+    @Transactional
     //requisição do tipo post para a URL /medicos, para que ative o método cadastrar da classe MedicoController
     public void cadastrar(@RequestBody DadosCadastroMedico dados) {
         repository.save(new Medico(dados));
